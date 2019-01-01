b5c05f37d95f59f7e872c18aa6341116cce00a6b
Scott Ian <sim2487@gmail.com>
2019-01-01 09:21:22 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const { PORT, CLIENT_ORIGIN } = require('./config');
+const { dbConnect } = require('./db-mongoose');
+
+const app = express();
+
+app.use(
+  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+    skip: (req, res) => process.env.NODE_ENV === 'test'
+  })
+);
+
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
+
+app.get('/api/stat', (req, res) => {
+  return res.json({
+    hello: "there",
+  });
+});
+
+function runServer(port = PORT) {
+  const server = app
+    .listen(port, () => {
+      console.info(`App listening on port ${server.address().port}`);
+    })
+    .on('error', err => {
+      console.error('Express failed to start');
+      console.error(err);
+    });
+}
+
+if (require.main === module) {
+  dbConnect();
+  runServer();
+}
+
+module.exports = { app };
